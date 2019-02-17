ddf3c35352f2515564c03d89c74cf070e4e646e9
Nigar <movsum.nigar@gmail.com>
2019-02-18 00:43:07 +0400

    Changed LoadingComponent
    0. Loading Component now receives a message from part of the code it is used at. 
    1. added default props in case if we don't have a message for loadingcomponent
    2. deleted useless divs and classNames to make less mess.

--- src/components/LoadingComponent.jsx
+++ src/components/LoadingComponent.jsx
@@ -1,25 +1,18 @@
 import React from 'react';
 
-const LoadingComponent = () => {
+
+const LoadingComponent= (props)=>{
     return(
-                <div className="ui segment">
-                    <div className="ui active transition visible dimmer">
-                        <div className="content">
-                            <div className="ui massive text loader">Loading</div>
-                        </div>
-                    </div>
-                    <img alt="" src="https://react.semantic-ui.com/images/wireframe/short-paragraph.png"
-                         className="ui image"/>
-                    <img alt="" src="https://react.semantic-ui.com/images/wireframe/short-paragraph.png"
-                         className="ui image"/>
-                    <img alt="" src="https://react.semantic-ui.com/images/wireframe/short-paragraph.png"
-                    className="ui image"/>
-                </div>
+        <div className="ui active dimmer">
+            <div className="ui big text loader">{props.message}</div>
+        </div>
+
+
     );
-
-
 }
+
+LoadingComponent.defaultProps = {
+    message: 'Loading...'
+};
+
 export default LoadingComponent;
-
-
-
